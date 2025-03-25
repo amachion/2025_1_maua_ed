6289da049565047459ded151ffd0ad62a7ecd2a0
Andreia Machion <74379473+amachion@users.noreply.github.com>
2025-03-25 11:31:48 -0300

    Tipos abstratos de dados
    aula 25 de março

---
+++ teo1/tads/TesteZero.java
@@ -0,0 +1,7 @@
+public class TesteZero {
+    public static void main (String[] args) {
+        //System.out.print("divisão inteira: " + 1/0);
+        double x = 1, y = 0;
+        System.out.print("divisao double: " + x/y);
+    }
+}
