5eb9618d55c55854c874b2bdb2af0daeb4977976
Andreia Machion <74379473+amachion@users.noreply.github.com>
2025-03-26 13:06:18 -0300

    correção exercícios
    aula de 26 de março

--- t2_sub1/lista_simples/ListaSimples.java
+++ t2_sub1/lista_simples/ListaSimples.java
@@ -71,15 +71,59 @@ public class ListaSimples {
 
     public int getTamanho() {
         //retorna o tamanho da lista
+        if (estaVazia()) return 0;
+        No runner = primeiro;
+        int cont = 1;
+        while (runner != null) {
+            cont++;
+            runner = runner.getProximo();
+        }
+        return cont;
     }
     public void inserePosicao (int elemento, int posicao) {
         //insere o elemento na posição pedida
         //considere a primeira como posição 1
         //se posicao < 1 -> insere no inicio
         //se podicao > tamanho -> insere no final
+        //cenário 1: lista vazia ou posição <= 1
+        if (estaVazia() || posicao <= 1) {
+            insereInicio(elemento);
+        }
+        else {
+            int pos = 1;
+            No runner = primeiro;
+            while (runner != null && pos < posicao-1) {
+                runner = runner.getProximo();
+                pos++;
+            }
+            //descobrir o porque saiu do laço
+            if (runner == null) {
+                insereFim(elemento);
+            }
+            else {
+                No novo = new No(elemento);
+                novo.setProximo(runner.getProximo());
+                runner.setProximo(novo);
+            }
+        }
     }
     public boolean removeElemento(int elemento) {
         //encontra a primeira ocorrencia do elemento e remove da lista, retornando sucess
         //se não encontrar, retorna fracasso
+        if (primeiro.getInfo() == elemento) {
+            removeInicio();
+            return true;
+        }
+        No anterior = primeiro;
+        No atual = primeiro.getProximo();
+        while (atual != null && atual.getInfo() != elemento) {
+            anterior = atual;
+            atual = atual.getProximo();
+        }
+        //por que saiu do laço?
+        if (atual == null) return false;
+        //achou o elemento na posição atual
+        anterior.setProximo(atual.getProximo());
+        return true;
     }
 }
