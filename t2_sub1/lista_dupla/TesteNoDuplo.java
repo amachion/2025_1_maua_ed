5b5449a69b3d682034666e2ed879f85c95a7890b
Andreia Machion <74379473+amachion@users.noreply.github.com>
2025-03-26 13:07:15 -0300

    inicio lista duplamente ligada
    aula de 26 de março

---
+++ t2_sub1/lista_dupla/TesteNoDuplo.java
@@ -0,0 +1,17 @@
+public class TesteNoDuplo {
+    public static void main(String[] args) {
+        NoDuplo no1 = new NoDuplo(10);
+        NoDuplo no2 = new NoDuplo(20);
+
+        System.out.println("no1: " + no1);
+        System.out.println("no2: " + no2);
+
+        no1.setProximo(no2);
+        no2.setAnterior(no1);
+
+        no1.getProximo().setInfo(500);
+        no2.getAnterior().setInfo(1000);
+        System.out.println("no1: " + no1);
+        System.out.println("no2: " + no2);
+    }
+}
