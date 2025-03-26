315fd4696fb513496c4bf36bf8b93741d2d11269
Andreia Machion <74379473+amachion@users.noreply.github.com>
2025-03-26 09:37:49 -0300

    aula de 26 de março
    resolução dos exercícios

--- t1_sub3/listas_ligadas_simples/ListaSimples.java
+++ t1_sub3/listas_ligadas_simples/ListaSimples.java
@@ -69,8 +69,51 @@ public class ListaSimples {
     public void inserePosicao (int elemento, int posicao){
         //busca a posição e faz a inserção do elemento
         //a primeira posição é 1
+        //cenário 1: posição <= 1
+        if (estaVazia() || posicao <= 1) {
+            insereInicio(elemento);
+        }
+        else {
+            //buscar a posição sem sair fora da lista
+            No runner = primeiro;
+            int pos = 1;
+            while (runner != ultimo && pos < posicao-1) {
+                pos++;
+                runner = runner.getProximo();
+            }
+            //por que saiu do laço
+            if (pos == posicao-1) {
+                //achei a posição no meio da lista
+                No novo = new No(elemento);
+                novo.setProximo(runner.getProximo());
+                runner.setProximo(novo);
+            }
+            else {
+                //chegamos ao último da lista
+                insereFim(elemento);
+            }
+        }
     }
     public boolean removeElemento (int elemento) {
         //busca o elemento e o remove da lista se o encontrar
+        //cenário 1: é o primeiro
+        if (primeiro.getInfo() == elemento) {
+            removeInicio();
+            return true;
+        }
+        //vamos usar 2 ponteiros para varrer a lista
+        No anterior = primeiro;
+        No atual = primeiro.getProximo(); //é o segundo
+        while (atual != null && atual.getInfo() != elemento) {
+            anterior = atual;
+            atual = atual.getProximo();
+        }
+        //por que saímos do laço?
+        if (atual == null) { //elemento não encontrado
+            return false;
+        }
+        //achamos o elemento
+        anterior.setProximo(atual.getProximo());
+        return true;
     }
 }
--- t1_sub3/listas_ligadas_simples/TesteLista.java
+++ t1_sub3/listas_ligadas_simples/TesteLista.java
@@ -28,5 +28,27 @@ public class TesteLista {
         lista.insereFim(12);
         lista.insereFim(30);
         System.out.println("lista depois das insercoes de fim\n" + lista);
+
+        lista.inserePosicao(200, 0);
+        System.out.println("inseri o 200 na pos 0\n" + lista);
+
+        lista.inserePosicao(300, 3);
+        System.out.println("inseri o 300 na pos 3\n" + lista);
+
+        lista.inserePosicao(400, 10);
+        System.out.println("inseri o 400 na pos 10\n" + lista);
+
+        //o ideal é chamar as remoções de elemento usando if-else
+        lista.removeElemento(200);
+        System.out.println("removi o 200\n" + lista);
+
+        lista.removeElemento(5);
+        System.out.println("removi o 5\n" + lista);
+
+        lista.removeElemento(400);
+        System.out.println("removi o 400\n" + lista);
+
+        lista.removeElemento(500);
+        System.out.println("removi o 500\n" + lista);
     }
 }
