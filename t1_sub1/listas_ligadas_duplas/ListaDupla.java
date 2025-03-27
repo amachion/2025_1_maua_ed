e0bfe8a40597eb57872ae32938ec443fc5ffcd15
Andreia Machion <74379473+amachion@users.noreply.github.com>
2025-03-27 09:17:10 -0300

    completamos a lista dupla
    aula de 27 de março

--- t1_sub1/listas_ligadas_duplas/ListaDupla.java
+++ t1_sub1/listas_ligadas_duplas/ListaDupla.java
@@ -29,8 +29,45 @@ public class ListaDupla {
                 s+= " = " + runner ;
                 runner = runner.getProximo();
             }
-            s += "= \\\\";
+            s += " = \\\\";
         }
         return s + "\n";
+    }
+    public void insereFim (int info) {
+        NoDuplo novo = new NoDuplo(info);
+        if (listaVazia()) {
+            primeiro = novo;
+        }
+        else {
+            novo.setAnterior(ultimo);
+            ultimo.setProximo(novo);
+        }
+        ultimo = novo;
+    }
+    public int removeInicio()  {
+        if (listaVazia()) throw new RuntimeException("lista vazia, nao ha o que remover");
+        int info = primeiro.getInfo();
+        if (primeiro == ultimo) {
+            primeiro = null;
+            ultimo = null;
+        }
+        else {
+            primeiro = primeiro.getProximo();
+            primeiro.setAnterior(null);
+        }
+        return info;
+    }
+    public int removeFim()  {
+        if (listaVazia()) throw new RuntimeException("lista vazia, nao ha o que remover");
+        int info = ultimo.getInfo();
+        if (primeiro == ultimo) {
+            primeiro = null;
+            ultimo = null;
+        }
+        else {
+            ultimo = ultimo.getAnterior();
+            ultimo.setProximo(null);
+        }
+        return info;
     }
 }
--- t1_sub1/listas_ligadas_duplas/TesteListaDupla.java
+++ t1_sub1/listas_ligadas_duplas/TesteListaDupla.java
@@ -1,12 +1,49 @@
+import java.util.Random;
+
 public class TesteListaDupla {
     public static void main(String[] args) {
         ListaDupla ld = new ListaDupla();
-        System.out.println("lista instanciada: " + ld);
+        // System.out.println("lista instanciada: " + ld);
 
-        ld.insereInicio(10);
-        ld.insereInicio(20);
-        ld.insereInicio(30);
+        // ld.insereInicio(10);
+        // ld.insereInicio(20);
+        // ld.insereInicio(30);
 
-        System.out.println("lista depois das insercoes de inicio:" + ld);
+        // System.out.println("lista depois das insercoes de inicio:\n" + ld);
+
+        // ld.insereFim(15);
+        // ld.insereFim(25);
+        // ld.insereFim(35);
+
+        // System.out.println("lista depois das insercoes de fim:\n" + ld);
+
+        // System.out.println(ld.removeInicio() + " saiu do inicio");
+        // System.out.println(ld.removeFim() + " saiu do final");
+        // System.out.println("lista depois das remocoes:\n" + ld);
+        Random r = new Random();
+        for (int i=1; i<=100; i++) {
+            if (r.nextBoolean()) {//insere
+                if (r.nextBoolean()) { //no início
+                    ld.insereInicio(r.nextInt(100));
+                }
+                else { //no final
+                    ld.insereFim(r.nextInt(100));
+                }
+            }
+            else {//remove
+                try {
+                    if (r.nextBoolean()) { //no início
+                        System.out.println(ld.removeInicio() + " saiu do inicio");
+                    }
+                    else { // no final
+                        System.out.println(ld.removeFim() + " saiu do final");
+                    }
+                }
+                catch (RuntimeException rt) {
+                    System.out.println(rt.getMessage());
+                }
+            }
+            System.out.println(ld);
+        }
     }
 }
