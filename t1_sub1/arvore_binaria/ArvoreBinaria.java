aa69d9a28db4fb32f359236f4c6707c444872a59
Andreia Machion <74379473+amachion@users.noreply.github.com>
2025-04-23 13:36:49 -0300

    árvores binárias de busca
    aula 22 de abril

---
+++ t1_sub1/arvore_binaria/ArvoreBinaria.java
@@ -0,0 +1,29 @@
+public class ArvoreBinaria {
+    private No raiz;
+    //construtor padrão: raiz = null
+    public boolean arvoreVazia() {
+        return raiz == null;
+    }
+    public void insere (int info) {
+        No novo = new No(info);
+        if (arvoreVazia())
+            raiz = novo;
+        else 
+            insereRec(novo, raiz);
+    }
+    private void insereRec (No novo, No atual) {
+        if (novo.getInfo() > atual.getInfo()) {
+            if (atual.getDireita() == null)
+                atual.setDireita(novo);
+            else {
+                insereRec(novo, atual.getDireita());
+            }
+        }
+        else {
+            if (atual.getEsquerda() == null) 
+                atual.setEsquerda(novo);
+            else
+                insereRec(novo, atual.getEsquerda());
+        }
+    }
+}
