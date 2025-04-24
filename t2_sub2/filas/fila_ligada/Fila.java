6dd7e0bad81685b14483ef54d97065a2b68f1351
Andreia Machion <74379473+amachion@users.noreply.github.com>
2025-04-24 12:53:56 -0300

    filas sobre vetores e listas ligadas
    aula 24 de abril

---
+++ t2_sub2/filas/fila_ligada/Fila.java
@@ -0,0 +1,55 @@
+public class Fila {
+    private No primeiro;
+    private No ultimo;
+    //construtor padrão
+    public boolean filaVazia() {
+        return primeiro == null;
+    }
+    public void enfileira (int info) {
+        No novo = new No(info);
+        if (filaVazia())
+            primeiro = novo;
+        else 
+            ultimo.setProximo(novo);
+        ultimo =  novo;
+    }
+    public int desenfileira () {
+        if (filaVazia()) throw new RuntimeException("falha no desenfileiramento");
+        int temp = primeiro.getInfo();
+        primeiro = primeiro.getProximo();
+        if (primeiro == null) //esvaziou a fila
+            ultimo = null; //corta todas as referências
+        return temp;
+    }
+    @Override
+    public String toString () {
+        if (filaVazia()) return "fila vazia";
+        String s = "";
+        No runner = primeiro;
+        while (runner != null) {
+            s += runner + " -> ";
+            runner = runner.getProximo();
+        }
+        return s + "\\\\";
+    }
+}
+class No {
+    private int info;
+    private No proximo;
+    public No(int info) {
+        this.info = info;
+    }
+    public int getInfo() {
+        return info;
+    }
+    public No getProximo() {
+        return proximo;
+    }
+    public void setProximo(No proximo) {
+        this.proximo = proximo;
+    }
+    @Override
+    public String toString () {
+        return "[" + info + "]";
+    }
+}
---
+++ t2_sub2/filas/fila_vetor/TesteFilaVetor.java
@@ -0,0 +1,23 @@
+import java.util.Random;
+
+public class TesteFilaVetor {
+    public static void main(String[] args) {
+        Random random = new Random();
+        Fila fila = new Fila(8);
+        for (int i=1; i<=20; i++) {
+            try {
+                if (random.nextBoolean()){
+                    fila.enfileira(random.nextInt(10));
+                }
+                else {
+                    System.out.println(fila.desenfileira() + " saiu");
+                }
+                System.out.println(fila);
+                System.out.println(fila.stringVetor());
+            }
+            catch (RuntimeException rt) {
+                System.out.println(rt.getMessage());
+            }
+        }
+    }
+}
