b5f174214d6946e08760008bd435e478aea5272a
Andreia Machion <74379473+amachion@users.noreply.github.com>
2025-05-22 12:28:42 -0300

    complemento hash
    aula 22 de maio

--- t2_sub2/hash/TesteHash.java
+++ t2_sub2/hash/TesteHash.java
@@ -1,14 +1,37 @@
 public class TesteHash {
     public static void main(String[] args) {
         NossoHash<Integer, String> mapa = new NossoHash<>();
-        mapa.put(new Integer("1234"), "aaaaaa");
-        mapa.put(new Integer("4567"), "bbbbbbb");
-        mapa.put(new Integer("876543"), "werty");
-        mapa.put(new Integer("23456"), "oiuyg");
-        mapa.put(new Integer("765"), "mnbvc");
-        mapa.put(new Integer("23956"), "iuy");
-        mapa.put(new Integer("0987"), "werty");
-        mapa.put(new Integer("34576"), "jbvc");
+        mapa.put(1234, "aaaaaa");
+        mapa.put(4567, "bbbbbbb");
+        mapa.put(876543, "werty");
+        mapa.put(23456, "oiuyg");
+        mapa.put(765, "mnbvc");
+        mapa.put(23956, "iuy");
+        mapa.put(987, "werty");
+        mapa.put(34576, "jbvc");
+
         mapa.imprime();
+
+        String valor = mapa.get(23956);
+        if (valor != null) {
+            System.out.println("Chave encontrada, valor = " + valor);
+        }
+        else {
+            System.out.println("chave nao encontrada");
+        }
+
+        if (mapa.containsKey(4567)) {
+            System.out.println("chave encontrada");
+        }
+        else {
+            System.out.println("chave NAO encontrada");
+        }
+        if (mapa.containsValue("FFFFF")) {
+            System.out.println("valor encontrado");
+        }
+        else {
+            System.out.println("valor NAO encontrado");
+        }
+        System.out.println("tamanho da estrutura: " + mapa.size());
     }
 }
